Fixed velocity normalization properly

pytorch_model/data_loader.py
@@ -293,6 +293,15 @@ def load_all_trajectories(tfrecord_path, meta_path, max_trajs):
         # print(f"Loaded trajectory {traj_idx}")
 
     mean = sum_elements / element_num
+
+    # Common normalization for velocity and position, mean
+    # # Position
+    # shared_mean_pos = mean[0:3].mean()
+    # mean[0:3] = shared_mean_pos
+    # # Velocity (cols 5,6,7)
+    # shared_mean_vel = mean[5:8].mean()
+    # mean[5:8] = shared_mean_vel
+
     std_acc = torch.zeros_like(mean)
     for traj in list_of_trajs:
         X = traj['X_seq_norm']
@@ -300,7 +309,7 @@ def load_all_trajectories(tfrecord_path, meta_path, max_trajs):
 
     std_dev = torch.sqrt(std_acc / (element_num - 1))
 
-    # TODO NORMALIZING ALL THREE COMPONENTS IN AN EQUIVALENT WAY
+    # Common normalization for position and velocity components, stdev
     max_std_pos = std_dev[0:3].max()
     std_dev[0:3] = max_std_pos
     max_std_vel = std_dev[5:8].max()